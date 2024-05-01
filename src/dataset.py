f706997ff3895cb710563abc8cfaf0f2fdd4349c
Alexandre Robin <alexandre.robin81@gmail.com>
2024-05-01 12:44:36 +0200

    Dataset

--- src/dataset.py
+++ src/dataset.py
@@ -8,17 +8,30 @@ from torch.utils.data import Dataset
 DATA_PATH = "../imagenette"
 DATA_SUMMARY = "noisy_imagenette.csv"
 
-LABEL_DIC = {
-    "n01440764": "Tench",
-    "n02102040": "English springer",
-    "n02979186": "Cassette player",
-    "n03000684": "Chainsaw",
-    "n03028079": "Church",
-    "n03394916": "French horn",
-    "n03417042": "Garbage truck",
-    "n03425413": "Gas pump",
-    "n03445777": "Golf ball",
-    "n03888257": "Parachute",
+LABEL_ID_DIC = {
+    "n01440764": 0,
+    "n02102040": 1,
+    "n02979186": 2,
+    "n03000684": 3,
+    "n03028079": 4,
+    "n03394916": 5,
+    "n03417042": 6,
+    "n03425413": 7,
+    "n03445777": 8,
+    "n03888257": 9,
+}
+
+LABEL_NAME_DIC = {
+    0: "Tench",
+    1: "English springer",
+    2: "Cassette player",
+    3: "Chainsaw",
+    4: "Church",
+    5: "French horn",
+    6: "Garbage truck",
+    7: "Gas pump",
+    8: "Golf ball",
+    9: "Parachute",
 }
 
 
@@ -39,8 +52,10 @@ class ImageDataset(Dataset):
         image = Image.open(osp.join(DATA_PATH, image_file)).convert("RGB")
 
         label = self.data_summary.loc[idx, "noisy_labels_0"]
+        one_hot_label = torch.zeros(len(LABEL_ID_DIC.keys()))
+        one_hot_label[LABEL_ID_DIC[label]] = 1
 
         if self.transform:
             image = self.transform(image)
 
-        return image, label
+        return image, one_hot_label
